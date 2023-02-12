2f4e72ac1944e4ceb429ad10981e21027aaa44c5
y-kolyada <yakov.kolyada@gmail.com>
2023-02-12 20:20:51 +0200

    terrafrom vpc_sec_gr  main.cf

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,6 +16,8 @@ provider "aws" {
 resource "aws_instance" "web-04" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
+  key_name      = "aws-web"
+  vpc_security_group_ids  = [sg-0b91374a598a160c5,sg-0493ee599c4918dcc]
 
   tags = {
     Name = var.instance_name
