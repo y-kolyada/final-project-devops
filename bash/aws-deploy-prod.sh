46ce65fc3d6d35c534a7305f363c256ec7152da3
Yakov Kolyada <yakov.kolyada@gmail.com>
2023-02-22 21:21:13 +0200

    bash aws-deploy-prod.sh remote run upd

--- bash/aws-deploy-prod.sh
+++ bash/aws-deploy-prod.sh
@@ -3,7 +3,20 @@
 # aws-deploy-prod.sh
 # updated: 2023-02-14
 
+o="StrictHostKeyChecking=off"
+app_src=$FINAL_PROJECT_DEVOPS_HOME
+app_path="$HOME"
+
 cd $FINAL_PROJECT_DEVOPS_HOME/terraform
+
 aws_host=$(cat ./prod_public_ip)
 
-echo $aws_host
+echo "AWS instance public_ip: $aws_host"
+ssh -o ${o} devops@${aws_host} mkdir -p $app_path/bin
+ssh -o ${o} devops@${aws_host} mkdir -p $app_path/log
+scp -o ${o} ../bin/devopskb.jar devops@${aws_host}:${app_path}/bin/
+
+#scp -o ${o} ../bash/aws_runapp_devopskb.sh devops@${aws_host}:${app_path}/bin/
+#scp -o ${o} ../bash/aws_stopapp_devopskb.sh devops@${aws_host}:${app_path}/bin/
+
+ssh -o ${o} devops@${aws_host} "cd ${app_path}/bin; ./aws_runapp_devopskb.sh"
