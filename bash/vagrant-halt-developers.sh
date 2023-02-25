cff3def7d55963e41ab7a0ac7c71a5e7b4c10a17
Yakov Kolyada <yakov.kolyada@gmail.com>
2023-02-25 22:57:51 +0200

    vagrant vagrant-halt-developers add qa

--- bash/vagrant-halt-developers.sh
+++ bash/vagrant-halt-developers.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # vagrant-halt-developers.sh
-# updated: 2023-02-03
+# updated: 2023-02-25
 
 cd $FINAL_PROJECT_DEVOPS_HOME/vagrant
 vagrant halt dev001
+vagrant halt qa001
 vagrant status
